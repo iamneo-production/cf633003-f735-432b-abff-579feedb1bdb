9cd6ed22c854fba9a888587c58f86884412cba32
Sumit-5049 <sumitspatil67@gmail.com>
2023-08-06 11:47:26 +0530

    V 1.0.0 - Auth ,Booking Completed

--- healthcare-appointment-booking/appointment-schedular-service/src/main/java/com/hackathon/appointmentschedularservice/dto/Date.java
+++ healthcare-appointment-booking/appointment-schedular-service/src/main/java/com/hackathon/appointmentschedularservice/dto/Date.java
@@ -17,5 +17,26 @@ import lombok.ToString;
 public class Date {
     private LocalDate date;
     private String time;
-    
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public Date(LocalDate date, String time) {
+		super();
+		this.date = date;
+		this.time = time;
+	}
+	public Date() {}
+	@Override
+	public String toString() {
+		return "Date [date=" + date + ", time=" + time + "]";
+	}
 }
--- healthcare-appointment-booking/appointment-status-service/src/main/java/com/hackathon/appointmentstatusservice/entity/AppointmentStatus.java
+++ healthcare-appointment-booking/appointment-status-service/src/main/java/com/hackathon/appointmentstatusservice/entity/AppointmentStatus.java
@@ -31,6 +31,114 @@ public class AppointmentStatus {
     private LocalDate date;
     private String time;
     private Status status;
+	public int getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(int appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	public int getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+	public int getProviderId() {
+		return providerId;
+	}
+	public void setProviderId(int providerId) {
+		this.providerId = providerId;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public AppointmentStatus(int appointmentId, int patientId, int providerId, LocalDate date, String time,
+			Status status) {
+		super();
+		this.appointmentId = appointmentId;
+		this.patientId = patientId;
+		this.providerId = providerId;
+		this.date = date;
+		this.time = time;
+		this.status = status;
+	}
+	public AppointmentStatus() {}
+	@Override
+	public String toString() {
+		return "AppointmentStatus [appointmentId=" + appointmentId + ", patientId=" + patientId + ", providerId="
+				+ providerId + ", date=" + date + ", time=" + time + ", status=" + status + "]";
+	}
+	 public static Builder builder() {
+	        return new Builder();
+	    }
+	public static class Builder {
+        private int appointmentId;
+        private int patientId;
+        private int providerId;
+        private LocalDate date;
+        private String time;
+        private Status status;
+
+        private Builder() {
+            // Private constructor to be used only by the outer class.
+        }
+
+        public Builder appointmentId(int appointmentId) {
+            this.appointmentId = appointmentId;
+            return this;
+        }
+
+        public Builder patientId(int patientId) {
+            this.patientId = patientId;
+            return this;
+        }
+
+        public Builder providerId(int providerId) {
+            this.providerId = providerId;
+            return this;
+        }
+
+        public Builder date(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder time(String time) {
+            this.time = time;
+            return this;
+        }
+
+        public Builder status(Status status) {
+            this.status = status;
+            return this;
+        }
+
+        public AppointmentStatus build() {
+            AppointmentStatus appointmentStatus = new AppointmentStatus();
+            appointmentStatus.appointmentId = this.appointmentId;
+            appointmentStatus.patientId = this.patientId;
+            appointmentStatus.providerId = this.providerId;
+            appointmentStatus.date = this.date;
+            appointmentStatus.time = this.time;
+            appointmentStatus.status = this.status;
+            return appointmentStatus;
+        }
+    }
 
     
 }
